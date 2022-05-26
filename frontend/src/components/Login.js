031f840a2b86ba3c884dd4d21ba1052128f64e4a
frida axelsson <axelsson.frida@gmail.com>
2022-05-26 15:07:10 +0200

    added logged in mode and login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,33 +1,79 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector, batch } from "react-redux";
+
+import { API_URL } from "utils/utils";
 
 const Login = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const onFormSubmit = ()=> {
-        
-    }
-  return(
-      <>
+  const [mode, setMode] = useState("register");
+
+  const dispatch = useDispatch();
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    };
+
+    fetch(API_URL(mode), options)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.success) {
+                    batch(()=> {
+                        dispatch(user.actions.setUserId(data.userId));
+                        dispatch(user.actions.setAccessToken(data.accessToken));
+                        dispatch(user.actions.setUserName(data.username));
+                        dispatch(user.actions.setError(null));
+                    });
+                   
+                } else {
+                    batch(()=> {
+                        dispatch(user.actions.setError(data.response));
+                        dispatch(user.actions.setUserId(null));
+                        dispatch(user.actions.setAccessToken(null));
+                        dispatch(user.actions.setUserName(null));
+                    });
+                   
+                }
+            })
+  }
+
+  return (
+    <>
+    <label htmlFor="register">Register</label>
+    <input type ="radio" id="register" checked={mode === "register"} onChange={()=> setMode("register")}/>
+
+    <label htmlFor="login">Login</label>
+    <input type="radio" id="login" checked={mode === "login"} onChange={()=> setMode("login")}/>
+
       <form onSubmit={onFormSubmit}>
-          <label htmlFor="username">Username</label>
-          <input
+        <label htmlFor="username">Username</label>
+        <input
           type="text"
           id="username"
           value={username}
-          onChange={(e)=>setUsername(e.target.value)}/>
+          onChange={(event) => setUsername(event.target.value)}
+        />
 
-          <label htmlFor="password">Password</label>
-          <input
-          type="text"
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
           id="password"
           value={password}
-          onChange={(e)=>setPassword(e.target.value)}/>
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-          <button type="submit">Submit</button>
+        <button type="submit">Submit</button>
       </form>
-
-      </>
-  )
+    </>
+  );
 };
 export default Login;
