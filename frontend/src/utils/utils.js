ebd51bd1330a3a583a08e1b56fc4a36aedb699ab
frida axelsson <axelsson.frida@gmail.com>
2022-05-27 15:35:49 +0200

    changed base-url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,4 @@
-const BASE_URL="http://localhost:8080";
+//const BASE_URL="http://localhost:8080";
+const BASE_URL="https://project-auth-frida-sara-jacob.herokuapp.com/";
 
 export const API_URL = (slug) =>`${BASE_URL}/${slug}`;
