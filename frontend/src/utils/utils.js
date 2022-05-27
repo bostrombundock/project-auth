2e2d1311a62e48a5b8a8e76ebf9d1d1a85884261
frida axelsson <axelsson.frida@gmail.com>
2022-05-27 15:45:18 +0200

    make it better

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,4 @@
 //const BASE_URL="http://localhost:8080";
-const BASE_URL="https://project-auth-frida-sara-jacob.herokuapp.com/";
+const BASE_URL="https://project-auth-frida-sara-jacob.herokuapp.com";
 
 export const API_URL = (slug) =>`${BASE_URL}/${slug}`;
